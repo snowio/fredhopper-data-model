54409170d9503662c72b80e185142d3af2549825
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-03 11:38:05 +0100

    Update AttributeOption Value Object: Provide Implementation

--- src/AttributeOption.php
+++ src/AttributeOption.php
@@ -1,28 +1,41 @@
 <?php
 namespace SnowIO\FredhopperDataModel;
 
-
 class AttributeOption extends Entity
 {
     public static function of(string $optionId, array $labels): self
     {
+        $attributeOption = new self;
+        $attributeOption->optionId = $optionId;
+        $attributeOption->labels = $labels;
+        return $attributeOption;
     }
 
     public function getOptionId(): string
     {
+        return $this->optionId;
     }
 
     public function getLabels(): array
     {
+        return $this->labels;
     }
 
     public function getLabel($locale): ?string
     {
+        return $this->labels[$locale] ?? null;
     }
 
     public function toJson(): array
     {
+        $json = parent::toJson();
+        $json['option_id'] = $this->optionId;
+        $json['labels'] = $this->labels;
+        return $json;
     }
+
+    private $optionId;
+    private $labels;
 
     private function __construct()
     {
