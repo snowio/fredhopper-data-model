111b87f21922e1313537fff3cdab6c2d5ac2db52
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-03 11:28:42 +0100

    Add interface

---
+++ src/AttributeOption.php
@@ -0,0 +1,30 @@
+<?php
+namespace SnowIO\FredhopperDataModel;
+
+
+class AttributeOption extends Entity
+{
+    public static function of(string $optionId, array $labels): self
+    {
+    }
+
+    public function getOptionId(): string
+    {
+    }
+
+    public function getLabels(): array
+    {
+    }
+
+    public function getLabel($locale): ?string
+    {
+    }
+
+    public function toJson(): array
+    {
+    }
+
+    private function __construct()
+    {
+    }
+}
