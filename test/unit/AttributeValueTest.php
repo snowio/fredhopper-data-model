0712f2ef6ddd2738c9726c8a0f3ddf39919f44d5
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-03 08:30:03 +0100

    Add tests for Attribute Value Value Object

---
+++ test/unit/AttributeValueTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace SnowIO\FredhopperDataModel\Test;
+
+use Exception;
+use SnowIO\FredhopperDataModel\AttributeValue;
+
+class AttributeValueTest extends \PHPUnit\Framework\TestCase
+{
+    public function testObjectInitialisation()
+    {
+        $attributeValue = AttributeValue::of('color', 'Rot')->withLocale('de_DE');
+        self::assertEquals('color', $attributeValue->getAttributeId());
+        self::assertEquals('Rot', $attributeValue->getValue());
+        self::assertEquals('de_DE', $attributeValue->getLocale());
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid Id
+     */
+    public function testInvalidAttributeId()
+    {
+        AttributeValue::of('c%o*lour', 'Rose');
+    }
+
+    public function testToJson()
+    {
+        $attributeValue = AttributeValue::of('colour', 'Rot')->withLocale('de_DE');
+        self::assertEquals([
+            'colour' => 'Rot'
+        ], $attributeValue->toJson());
+    }
+
+    public function testSanitization()
+    {
+        $sanitizedValueId = AttributeValue::sanitizeId('red$');
+        self::assertEquals('red', $sanitizedValueId);
+    }
+}
