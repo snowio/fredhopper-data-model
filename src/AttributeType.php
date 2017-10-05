2a06b99d5d029235cc5c6cebdf3df75383a2ec35
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-05 08:59:17 +0100

    Add Attribute types: - Includes function to check if they are falid

---
+++ src/AttributeType.php
@@ -0,0 +1,33 @@
+<?php
+namespace SnowIO\FredhopperDataModel;
+
+final class AttributeType
+{
+    const FLOAT = 'float';
+    const INT = 'int';
+    const TEXT = 'text';
+    const LIST = 'list';
+    const SET = 'set';
+
+    const ALL = [
+        self::FLOAT,
+        self::INT,
+        self::TEXT,
+        self::LIST,
+        self::SET,
+    ];
+
+
+    public static function validateType(string $type)
+    {
+        $all = self::ALL;
+        if (!in_array($type, $all)) {
+            throw new \Exception('Invalid Type');
+        }
+    }
+
+    private function __construct()
+    {
+
+    }
+}
