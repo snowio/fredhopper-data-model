4718850f29e753b4a461312945e9d03422bcc712
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-05 08:54:23 +0100

    Update Variant Value Object: - Add get ProductId - Add variant id entry to toJson - Validate Id

--- src/Variant.php
+++ src/Variant.php
@@ -1,27 +1,30 @@
 <?php
 namespace SnowIO\FredhopperDataModel;
 
+use function SnowIO\FredhopperDataModel\Internal\validateId;
+
 class Variant extends Item
 {
     public static function of(string $id, string $productId): self
     {
-        validateId($id);
-        $variant = new self($productId);
-        $variant->variantId = $id;
+        validateId($productId);
+        $variant = new self($id);
+        $variant->productId = $productId;
         return $variant;
     }
 
-    public function getVariantId(): string
+    public function getProductId(): string
     {
-        return $this->variantId;
+        return $this->productId;
     }
 
     public function toJson(): array
     {
         $json = parent::toJson();
-        $json['variant_id'] = $this->variantId;
+        $json['variant_id'] = $this->getId();
+        $json['product_id'] = $this->productId;
         return $json;
     }
 
-    private $variantId;
+    private $productId;
 }
