4c2a04be010f794968f53282c77b64800aa0e269
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-18 15:40:20 +0100

    Update Test: Make test add implementation

--- test/unit/AttributeValueSetTest.php
+++ test/unit/AttributeValueSetTest.php
@@ -21,4 +21,32 @@ class AttributeValueSetTest extends \PHPUnit\Framework\TestCase
         self::assertCount(5, iterator_to_array($colors));
         self::assertEquals($colorArray, iterator_to_array($colors));
     }
+
+    public function testAddingAnAttributeSet()
+    {
+        $color = AttributeValue::of('color', 'red');
+        $size = AttributeValue::of('size', 'large');
+        $length = AttributeValue::of('length', 300);
+        $weight = AttributeValue::of('weight', 80);
+        $voltage = AttributeValue::of('voltage', 30);
+        $attributes = AttributeValueSet::of($attributeArray = [$color, $size, $length]);
+        $otherColors = AttributeValueSet::of($otherAttributeArray = [$weight, $voltage]);
+        $attributes = $attributes->add($otherColors);
+        $allAttributes = array_merge($attributeArray, $otherAttributeArray);
+        self::assertEquals($allAttributes, iterator_to_array($attributes));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testAddingTheSameAttributeSet()
+    {
+        $color = AttributeValue::of('color', 'red');
+        $size = AttributeValue::of('size', 'large');
+        $length = AttributeValue::of('length', 300);
+        $attributes = AttributeValueSet::of($attributeArray = [$color, $size, $length]);
+        $otherAttributes = AttributeValueSet::of($otherAttributeArray = [$color, $size, $length]);
+        $attributes = $attributes->add($otherAttributes);
+        $attributes->add($otherAttributes);
+    }
 }
